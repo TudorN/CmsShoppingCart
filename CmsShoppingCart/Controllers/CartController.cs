5a3233f6ba4fa2a2c87f6070715458ce94903925
Tudor Nedelcu <tudor.nedelcu@sogeti.com>
2019-02-25 15:12:55 +0100

    CartDetails added

---
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,78 @@
+﻿using CmsShoppingCart.Models.ViewModels.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            // Init the cart list
+            var cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+
+            // Check it cart is empty
+            if (cart.Count == 0 || Session["cart"] == null)
+            {
+                ViewBag.Message = "Your cart is empty.";
+                return View();
+            }
+
+            // Calculate total and save to ViewBag
+            decimal total = 0m;
+
+            foreach (var item in cart)
+            {
+                total += item.Total;
+            }
+
+            ViewBag.GrandTotal = total;
+
+            // Return view with list
+            return View();
+        }
+
+        public ActionResult CartPartial()
+        {
+            // Init CartVM
+            CartVM model = new CartVM();
+
+            // Init quantity
+            int qty = 0;
+
+            // Init price
+            decimal price = 0m;
+
+            // Check for cart session
+            if (Session["cart"] != null)
+            {
+                // Get total qty and price
+                var list = (List<CartVM>)Session["cart"];
+
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+            }
+            else
+            {
+                // Or set qty and price to 0
+                model.Quantity = 0;
+                model.Price = 0m;
+
+            }
+
+            // Return partial view with model
+            return PartialView(model);
+
+            
+        }
+
+        
+    }
+}
