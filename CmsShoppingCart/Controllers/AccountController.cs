81bf940330c3e40cf519bc94635d7e17afde18c3
Tudor Nedelcu <tudor.nedelcu@sogeti.com>
2019-02-26 15:04:51 +0100

    added login page

---
+++ CmsShoppingCart/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return Redirect("~/account/login");
+        }
+
+        // GET: /account/login
+        public ActionResult Login()
+        {
+            // Confirm user is not logged in
+            string username = User.Identity.Name;
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("user-profile");
+            }
+
+            // Return view
+           return View();
+        }
+        // GET: /account/create-account
+        [ActionName("create-account")]
+        public ActionResult CreateAccount()
+        {
+            return View("CreateAccount");
+        }
+    }
+}
